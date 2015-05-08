229c7e5f0c0075e23e6fe3e16a42aeb107fbd5c4
lannguyen <lannt@exoplatform.com>
2015-05-08 11:56:45 +0700

    Remove unnecessary notes and add more comments

--- lib/src/main/java/com/acme/samples/notification/SocialProfileListener.java
+++ lib/src/main/java/com/acme/samples/notification/SocialProfileListener.java
@@ -1,19 +1,3 @@
-/*
-	* Copyright (C) 2003-2015 eXo Platform SAS.
-	*
-	* This program is free software: you can redistribute it and/or modify
-	* it under the terms of the GNU Affero General Public License as published by
-	* the Free Software Foundation, either version 3 of the License, or
-	* (at your option) any later version.
-	*
-	* This program is distributed in the hope that it will be useful,
-	* but WITHOUT ANY WARRANTY; without even the implied warranty of
-	* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	* GNU Affero General Public License for more details.
-	*
-	* You should have received a copy of the GNU Affero General Public License
-	* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
 package com.acme.samples.notification;
 
 import org.exoplatform.commons.api.notification.NotificationContext;
@@ -24,12 +8,7 @@ import org.exoplatform.social.core.profile.ProfileLifeCycleEvent;
 import org.exoplatform.social.core.profile.ProfileListenerPlugin;
 import com.acme.samples.notification.plugin.UpdateProfilePlugin;
 
-/**
-	* Created by The eXo Platform SAS
-	* Author : eXoPlatform
-	*          thanhvc@exoplatform.com
-	* Jan 19, 2015  
-*/
+//This class extends ProfileListenerPlugin to trigger avatar/experience updating events and plug them into UpdateProfilePlugin as notifications
 public class SocialProfileListener extends ProfileListenerPlugin {
 	
 	@Override
@@ -45,7 +24,7 @@ public class SocialProfileListener extends ProfileListenerPlugin {
 		NotificationContext ctx = NotificationContextImpl.cloneInstance().append(UpdateProfilePlugin.PROFILE, profile);
 		ctx.getNotificationExecutor().with(ctx.makeCommand(PluginKey.key(UpdateProfilePlugin.ID))).execute(ctx);
 	}
-	
+	//Abstract classes must be overridden
 	@Override
 	public void contactSectionUpdated(ProfileLifeCycleEvent event) {}
 	
--- lib/src/main/java/com/acme/samples/notification/plugin/UpdateProfilePlugin.java
+++ lib/src/main/java/com/acme/samples/notification/plugin/UpdateProfilePlugin.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2003-2015 eXo Platform SAS.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
 package com.acme.samples.notification.plugin;
 
 import java.util.ArrayList;
@@ -33,53 +17,48 @@ import org.exoplatform.social.core.identity.model.Identity;
 import org.exoplatform.social.core.identity.model.Profile;
 import org.exoplatform.social.core.manager.RelationshipManager;
 
-/**
- * Created by The eXo Platform SAS
- * Author : eXoPlatform
- *          exo@exoplatform.com
- * Jan 21, 2015  
- */
+//This class extends BaseNotificationPlugin to push new notification type of profile updating event
 public class UpdateProfilePlugin extends BaseNotificationPlugin {
-  public final static ArgumentLiteral<Profile> PROFILE = new ArgumentLiteral<Profile>(Profile.class, "profile");
-  private static final Log LOG = ExoLogger.getLogger(UpdateProfilePlugin.class);
-  public final static String ID = "UpdateProfilePlugin";
-
-  public UpdateProfilePlugin(InitParams initParams) {
-    super(initParams);
-  }
-
-  @Override
-  public String getId() {
-    return ID;
-  }
-
-  @Override
-  public boolean isValid(NotificationContext ctx) {
-    return true;
-  }
-
-  @Override
-  protected NotificationInfo makeNotification(NotificationContext ctx) {
-    Profile profile = ctx.value(PROFILE);
-    Set<String> receivers = new HashSet<String>();
-    
-    RelationshipManager relationshipManager = CommonsUtils.getService(RelationshipManager.class);
-    Identity updatedIdentity = profile.getIdentity();
-    ListAccess<Identity> listAccess = relationshipManager.getConnections(updatedIdentity);
-    try {
-      Identity[] relationships =  relationshipManager.getConnections(updatedIdentity).load(0, listAccess.getSize());
-      for(Identity i : relationships) {
-        receivers.add(i.getRemoteId());
-      }
-    } catch (Exception ex) {
-      LOG.error(ex.getMessage(), ex);
-    }
-    
-    return NotificationInfo.instance()
-                           .setFrom(updatedIdentity.getRemoteId())
-                           .to(new ArrayList<String>(receivers))
-						   .setTitle(updatedIdentity.getProfile().getFullName() + " updated his/her profile.<br/>")
-                           .key(getId());
-  }
-
+	public final static ArgumentLiteral<Profile> PROFILE = new ArgumentLiteral<Profile>(Profile.class, "profile");
+	private static final Log LOG = ExoLogger.getLogger(UpdateProfilePlugin.class);
+	public final static String ID = "UpdateProfilePlugin";
+	
+	public UpdateProfilePlugin(InitParams initParams) {
+		super(initParams);
+	}
+	
+	@Override
+	public String getId() {
+		return ID;
+	}
+	
+	@Override
+	public boolean isValid(NotificationContext ctx) {
+		return true;
+	}
+	
+	@Override
+	protected NotificationInfo makeNotification(NotificationContext ctx) {
+		Profile profile = ctx.value(PROFILE);
+		Set<String> receivers = new HashSet<String>();
+		
+		RelationshipManager relationshipManager = CommonsUtils.getService(RelationshipManager.class);
+		Identity updatedIdentity = profile.getIdentity();
+		ListAccess<Identity> listAccess = relationshipManager.getConnections(updatedIdentity);
+		try {
+			Identity[] relationships =  relationshipManager.getConnections(updatedIdentity).load(0, listAccess.getSize());
+			for(Identity i : relationships) {
+				receivers.add(i.getRemoteId());
+			}
+			} catch (Exception ex) {
+			LOG.error(ex.getMessage(), ex);
+		}
+		
+		return NotificationInfo.instance()
+		.setFrom(updatedIdentity.getRemoteId())
+		.to(new ArrayList<String>(receivers))
+		.setTitle(updatedIdentity.getProfile().getFullName() + " updated his/her profile.<br/>")
+		.key(getId());
+	}
+	
 }
